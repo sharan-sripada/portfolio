31fc85480def3520302a64db9130620340325437
Sai Sharan <saisharanviper@gmail.com>
2024-09-28 16:51:28 -0400

    Image resize

--- app/components/homepage/about/index.jsx
+++ app/components/homepage/about/index.jsx
@@ -49,8 +49,6 @@ function AboutSection() {
             src="/profile.png"
             alt="SAI SHARAN Education"
             className="grayscale hover:grayscale-0 hover:scale-110 cursor-pointer object-cover"
-            // width={280}
-            // height={280}
       />
         </div>
       </div>

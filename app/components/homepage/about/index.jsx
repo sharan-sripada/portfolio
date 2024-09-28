fb89d97260358e53291e8a3df7afb72c4f33fe36
Sai Sharan <saisharanviper@gmail.com>
2024-09-27 20:29:55 -0400

    image path added

--- app/components/homepage/about/index.jsx
+++ app/components/homepage/about/index.jsx
@@ -24,7 +24,7 @@ function AboutSection() {
         </div>
         <div className="flex justify-center order-1 lg:order-2">
           <Image
-            src={personalData.profile}
+            src="/portfolio/profile.png"
             width={280}
             height={280}
             alt="SAI SHARAN"
